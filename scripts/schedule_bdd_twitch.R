08099e50347d79e57c860305c4503469604201b9
Heikel JAZI <heikel.j@live.fr>
2023-04-10 23:48:22 +0200

    script permettant d'exécuter automatiquement la récupération du top 100 des streams français au pas 5 minutes

---
+++ scripts/schedule_bdd_twitch.R
@@ -0,0 +1,31 @@
+# permet d'automatiser mon document
+library(taskscheduleR)
+
+# La méthode avec l'Addin et l'IHM issu de la librairie taskScheduler qui fonctionne bien 
+
+
+# Chemin où se situe le script (à changer lors d'un push dans git)
+path_script <- "chemin_vers_le_script"
+
+
+## Lance le script toutes les cinq minutes, commence à 23h40
+taskscheduler_create(taskname = "update_twitch_bdd",
+                     rscript = paste0(path_script,"BDD_twitch_scheduled.R"),
+                     schedule = "MINUTE",
+                     starttime = "23:40",
+                     modifier = 5)
+
+# Afficher la liste des tâches 
+# tasks <- taskscheduler_ls()
+# str(tasks)
+
+# fonction permettant de supprimer la tâche 
+taskscheduler_delete(taskname = "update_twitch_bdd")
+
+
+# nom de la tâche depuis l'IHM
+# taskscheduler_delete(taskname = "BDD_twitch_scheduled.R")
+
+
+
+
